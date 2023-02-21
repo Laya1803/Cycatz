762789157b635e9f74744bbbd009db830541119a
Layashree V <117625425+Laya1803@users.noreply.github.com>
2023-02-21 18:03:58 +0530

    Add files via upload

---
+++ Counter/Counter.js
@@ -0,0 +1,10 @@
+var count=0;
+function increment(){
+    count = count+1;
+    document.getElementById("counting").innerText = count;
+}
+
+function decrement(){
+    count = count-1;
+    document.getElementById("counting").innerText = count;
+}
