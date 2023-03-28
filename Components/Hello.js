96eebcd69efafe2f2a6de22af0db39e344555f2f
Layashree V <117625425+Laya1803@users.noreply.github.com>
2023-03-28 14:52:16 +0530

    Delete Components directory

--- Components/Hello.js
+++
@@ -1,8 +0,0 @@
-import React, {Component} from "react";
-class Hello extends Component{
-    render(){
-        const {name,heroName}=this.props
-        return <h2>Welcome {name} a.k.a {heroName}</h2>
-    }
-}
-export default Hello
--- Components/Welcome.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-const Welcome = () =>{
-    return React.createElement('div',null,React.createElement('h1',null,'Hello World'))
-}
-export default Welcome
